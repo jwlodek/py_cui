35323606f11af51d2b9a4f6f2520d67127158131
jwlodek <jwlodek@cs.stonybrook.edu>
2020-02-26 14:14:07 -0500

    Change doc generation script permissions

--- docs/scripts/updateDocs.sh
+++ docs/scripts/updateDocs.sh
@@ -9,7 +9,7 @@ rm -rf py_cui-docs
 git clone https://github.com/jwlodek/py_cui-docs
 fi
 cd py_cui
-pip3 install mkdocs
+pip3 install mkdocs==1.0.4
 python3 -m mkdocs build -d ../py_cui-docs
 cd ../py_cui-docs
 git add -A
