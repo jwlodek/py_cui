714364542df7db585d86227858ed7442d87398f0
Caleb Reese <calebdane.r@gmail.com>
2020-03-05 21:47:54 -0700

    Adding password parameter to travis text

--- tests/test_text_box.py
+++ tests/test_text_box.py
@@ -5,14 +5,14 @@ import py_cui
 grid_test = py_cui.grid.Grid(10, 10, 100, 100)
 
 def test_move_right():
-    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World')
+    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World', password = False)
     text_box.move_right()
     assert text_box.cursor_text_pos == 1
     assert text_box.cursor_x == 13
 
 
 def test_move_left_side():
-    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World')
+    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World', password = False)
     temp = text_box.cursor_x
     text_box.cursor_x = text_box.cursor_x + 5
     text_box.cursor_text_pos = 5
@@ -21,14 +21,14 @@ def test_move_left_side():
     assert text_box.cursor_x == (temp + 5 - 1)
 
 def test_clear():
-    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World')
+    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World', password = False)
     text_box.clear()
     assert text_box.get() == ''
     assert text_box.cursor_text_pos == 0
     assert text_box.cursor_x == 12
 
 def test_get_initial():
-    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World')
+    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World', password = False)
     assert text_box.cursor_text_pos == 0
     assert text_box.cursor_x == 12
     assert text_box.get() == 'Hello World'
@@ -38,14 +38,14 @@ def test_get_initial():
 
 
 def test_insert_char():
-    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World')
+    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World', password = False)
     text_box.insert_char(py_cui.keys.KEY_D_UPPER)
     assert text_box.get() == 'DHello World'
     assert text_box.cursor_x == text_box.cursor_max_left + 1
     assert text_box.cursor_text_pos == 1
 
 def test_erase_char():
-    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World')
+    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World', password = False)
     for i in range(0,2):
         text_box.move_right()
     text_box.erase_char()
@@ -54,7 +54,7 @@ def test_erase_char():
     assert text_box.get() == 'Hllo World'
 
 def test_get_edited():
-    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World')
+    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World', password = False)
     for i in range(0, 3):
         text_box.move_right()
     text_box.erase_char()
@@ -65,7 +65,7 @@ def test_get_edited():
     assert text_box.get() == 'HeaElo World'
 
 def test_jump_to_start():
-    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World')
+    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World', password = False)
     for i in range(0, 4):
         text_box.move_right()
     text_box.jump_to_start()
@@ -75,27 +75,27 @@ def test_jump_to_start():
 
 
 def test_jump_to_end():
-    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World')
+    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World', password = False)
     text_box.jump_to_end()
     assert text_box.cursor_text_pos == 11
     assert text_box.cursor_x == text_box.cursor_max_left + 11
 
 
 def test_move_right_overflow():
-    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 1, 1, 0 , 'Hello World!!!')
+    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 1, 1, 0 , 'Hello World!!!', password = False)
     for i in range(0, 20):
         text_box.move_right()
     assert text_box.cursor_text_pos == 14
     assert text_box.cursor_x == text_box.cursor_max_left + 5
 
 def test_move_left_overflow():
-    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World')
+    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World', password = False)
     text_box.move_left()
     assert text_box.cursor_text_pos == 0
     assert text_box.cursor_x == 12
 
 def test_set_text():
-    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World')
+    text_box = py_cui.widgets.TextBox('id', 'Test', grid_test, 1, 1, 1, 2, 1, 0 , 'Hello World', password = False)
     for i in range(0, 7):
         text_box.move_right()
     text_box.set_text('Hi')
