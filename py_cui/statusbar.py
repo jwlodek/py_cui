83083e6cb6e8a753b468b91ab73d5426c44486c4
jwlodek <jakub.wlodek@stonybrook.edu>
2020-03-19 17:34:08 -0400

    rework statusbar class

--- py_cui/statusbar.py
+++ py_cui/statusbar.py
@@ -19,15 +19,56 @@ class StatusBar:
     """
 
     def __init__(self, text, color):
-        """Constructor for statusbar
+        """Initializer for statusbar
         """
 
         self.__text = text
         self.__color = color
 
 
+    def get_color(self):
+        """Getter for status bar color
+        
+        Returns
+        -------
+        color : int
+            statusbar color
+        """
+
+        return self.__color
+
+
+    def get_text(self):
+        """Getter for stattus bar text
+
+        Returns
+        -------
+        text : str
+            The statusbar text
+        """
+
+        return self.__text
+
+
+    def set_color(self, color):
+        """Setter for statusbar color
+
+        Parameters
+        ----------
+        color : int
+            new statusbar color code
+        """
+
+        self.__color = color
+
+
     def set_text(self, text):
         """Sets the statusbar text
+
+        Parameters
+        ----------
+        text : str
+            New statusbar text
         """
 
         self.__text = text
