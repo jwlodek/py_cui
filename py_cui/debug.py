67402af3fd527b9cb7bb87ee8ceba9837fce0993
jwlodek <jakub.wlodek@stonybrook.edu>
2020-03-19 20:10:33 -0400

    Edits to debug module, logging works correctly

--- py_cui/debug.py
+++ py_cui/debug.py
@@ -7,13 +7,15 @@ import inspect
 import py_cui
 
 
-def _enable_logging(logger, logging_level=logging.DEBUG):
-    """Function that creates basic logging configuration
+def _enable_logging(logger, filename='py_cui_log.txt', logging_level=logging.DEBUG):
+    """Function that creates basic logging configuration for selected logger
 
     Parameters
     ----------
     logger : PyCUILogger
         Main logger object
+    filename : os.Pathlike
+        File path for output logfile
     logging_level : logging.LEVEL, optional
         Level of messages to display, by default logging.DEBUG
 
@@ -21,15 +23,18 @@ def _enable_logging(logger, logging_level=logging.DEBUG):
     ------
     PermissionError
         py_cui logs require permission to cwd to operate.
+    TypeError
+        Only the custom PyCUILogger can be used here.
     """
 
-    if not os.access('.', os.W_OK):
+    abs_path = os.path.abspath(filename)
+    if not os.access(os.path.dirname(abs_path), os.W_OK):
         raise PermissionError('You do not have permission to create py_cui.log file.')
 
     if not isinstance(logger, PyCUILogger):
         raise TypeError('Only the PyCUILogger can be used for logging in the py_cui module.')
 
-    log_file    = logging.FileHandler('py_cui.log')
+    log_file    = logging.FileHandler(filename)
     formatter   = logging.Formatter('%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s')
     log_file.setFormatter(formatter)
     logger.addHandler(log_file)
@@ -41,6 +46,8 @@ def _initialize_logger(py_cui_root, name=None, custom_logger=True):
     
     Parameters
     ----------
+    py_cui_root : py_cui.PyCUI
+        reference to the root py_cui window
     name : str, optional
         The name of the logger, by default None
     custom_logger : bool, optional
@@ -48,8 +55,8 @@ def _initialize_logger(py_cui_root, name=None, custom_logger=True):
     
     Returns
     -------
-    logger : logging.Logger
-        A logging.Logger
+    logger : py_cui.debug.PyCUILogger
+        A custom logger that allows for live debugging
     """
 
     if not custom_logger:
@@ -99,12 +106,25 @@ class PyCUILogger(logging.Logger):
         self.py_cui_root = py_cui_root
 
 
-    def __get_debug_text(self, text):
+    def _get_debug_text(self, text):
         """Function that generates full debug text for the log
         """
 
         func = inspect.currentframe().f_back.f_back.f_code
-        return "{}: {} in {}:{}".format(text, func.co_name, func.co_filename, func.co_firstlineno)
+        return "{}: Function {} in {}:{}".format(text, func.co_name, func.co_filename, func.co_firstlineno)
+    
+    
+    def info(self, text):
+        """Adds stacktrace info to log
+        
+        Parameters
+        ----------
+        text : str
+            The log text ot display
+        """
+
+        debug_text = self._get_debug_text(text)
+        super().debug(debug_text)
 
 
     def debug(self, text):
@@ -116,10 +136,11 @@ class PyCUILogger(logging.Logger):
             The log text ot display
         """
 
-        debug_text = self.__get_debug_text(text)
-        if self._live_debug_level > logging.DEBUG and self._live_debug_enabled:
-            self.py_cui_root.status_bar.set_text(debug_text)
-            super().debug(debug_text)
+        debug_text = self._get_debug_text(text)
+        if self._live_debug_level == logging.DEBUG and self._live_debug_enabled:
+            if self.py_cui_root is not None:
+                self.py_cui_root.status_bar.set_text(debug_text)
+                super().debug(debug_text)
         else:
             super().debug(debug_text)
 
@@ -133,10 +154,11 @@ class PyCUILogger(logging.Logger):
             The log text ot display
         """
 
-        debug_text = self.__get_debug_text(text)
-        if self._live_debug_level > logging.WARN and self._live_debug_enabled:
-            self.py_cui_root.status_bar.set_text(debug_text)
-            super().warn(debug_text)
+        debug_text = self._get_debug_text(text)
+        if self._live_debug_level < logging.WARN and self._live_debug_enabled:
+            if self.py_cui_root is not None:
+                self.py_cui_root.status_bar.set_text(debug_text)
+                super().debug(debug_text)
         else:
             super().warn(debug_text)
 
@@ -150,10 +172,11 @@ class PyCUILogger(logging.Logger):
             The log text ot display
         """
 
-        debug_text = self.__get_debug_text(text)
-        if self._live_debug_level > logging.ERROR and self._live_debug_enabled:
-            self.py_cui_root.status_bar.set_text(debug_text)
-            super().error(debug_text)
+        debug_text = self._get_debug_text(text)
+        if self._live_debug_level < logging.ERROR and self._live_debug_enabled:
+            if self.py_cui_root is not None:
+                self.py_cui_root.status_bar.set_text(debug_text)
+                super().debug(debug_text)
         else:
             super().error(debug_text)
 
