a991d919350722059728b3f5f78d75d3e7123eee
jwlodek <jakub.wlodek@stonybrook.edu>
2020-03-19 21:52:54 -0400

    Refactor unit tests to comply with new changes

--- tests/test_colors.py
+++ tests/test_colors.py
@@ -1,108 +1,113 @@
 import pytest
 import py_cui
+import py_cui.debug as dbg
 
+# Initialize debug logger
+logger = dbg.PyCUILogger('PYCUI TEST')
 
+# Some test strings
 test_string_A = "Hello world, etc 123 @ testing @ ++-- Test"
 test_string_B = "     Test string number two Space"
 test_string_C = "Hi"
 
-dummy_grid = py_cui.grid.Grid(3,3,30,30)
-dummy_widget = py_cui.widgets.Widget('1', 'Test', dummy_grid, 1,1,1,1,1,0)
-color_rule_A = py_cui.colors.ColorRule('@.*@', py_cui.RED_ON_BLACK, 'contains', 'regex', None, False)
-color_rule_B = py_cui.colors.ColorRule('Test', py_cui.RED_ON_BLACK, 'startswith', 'line', None, False)
-color_rule_C = py_cui.colors.ColorRule('Space', py_cui.RED_ON_BLACK, 'endswith', 'regex', None, False)
-color_rule_D = py_cui.colors.ColorRule('Test', py_cui.RED_ON_BLACK, 'notstartswith', 'region', [3,5], False)
-color_rule_E = py_cui.colors.ColorRule('Test', py_cui.RED_ON_BLACK, 'notendswith', 'line', None, False)
+# Initialize a dummy grid, widget, and color rules
+dummy_grid = py_cui.grid.Grid(3,3,30,30, logger)
+dummy_widget = py_cui.widgets.Widget('1', 'Test', dummy_grid, 1,1,1,1,1,0, logger)
+color_rule_A = py_cui.colors.ColorRule('@.*@',  py_cui.RED_ON_BLACK, 'contains',        'regex',    None,   False, logger)
+color_rule_B = py_cui.colors.ColorRule('Test',  py_cui.RED_ON_BLACK, 'startswith',      'line',     None,   False, logger)
+color_rule_C = py_cui.colors.ColorRule('Space', py_cui.RED_ON_BLACK, 'endswith',        'regex',    None,   False, logger)
+color_rule_D = py_cui.colors.ColorRule('Test',  py_cui.RED_ON_BLACK, 'notstartswith',   'region',   [3,5],  False, logger)
+color_rule_E = py_cui.colors.ColorRule('Test',  py_cui.RED_ON_BLACK, 'notendswith',     'line',     None,   False, logger)
 
 
 def test_check_match():
-    assert color_rule_A.check_match(test_string_A) == True
-    assert color_rule_B.check_match(test_string_A) == False
-    assert color_rule_C.check_match(test_string_A) == False
-    assert color_rule_D.check_match(test_string_A) == True
-    assert color_rule_E.check_match(test_string_A) == False
+    assert color_rule_A._check_match(test_string_A) == True
+    assert color_rule_B._check_match(test_string_A) == False
+    assert color_rule_C._check_match(test_string_A) == False
+    assert color_rule_D._check_match(test_string_A) == True
+    assert color_rule_E._check_match(test_string_A) == False
     
-    assert color_rule_A.check_match(test_string_B) == False
-    assert color_rule_B.check_match(test_string_B) == True
-    assert color_rule_C.check_match(test_string_B) == True
-    assert color_rule_D.check_match(test_string_B) == False
-    assert color_rule_E.check_match(test_string_B) == True
+    assert color_rule_A._check_match(test_string_B) == False
+    assert color_rule_B._check_match(test_string_B) == True
+    assert color_rule_C._check_match(test_string_B) == True
+    assert color_rule_D._check_match(test_string_B) == False
+    assert color_rule_E._check_match(test_string_B) == True
     
-    assert color_rule_A.check_match(test_string_C) == False
-    assert color_rule_B.check_match(test_string_C) == False
-    assert color_rule_C.check_match(test_string_C) == False
-    assert color_rule_D.check_match(test_string_C) == True
-    assert color_rule_E.check_match(test_string_C) == True
+    assert color_rule_A._check_match(test_string_C) == False
+    assert color_rule_B._check_match(test_string_C) == False
+    assert color_rule_C._check_match(test_string_C) == False
+    assert color_rule_D._check_match(test_string_C) == True
+    assert color_rule_E._check_match(test_string_C) == True
 
 
 def test_generate_fragments_A():
 
     fragments, match = color_rule_A.generate_fragments(dummy_widget, test_string_A, test_string_A)
     assert match == True
-    assert len(fragments) == 3
-    assert fragments[0][0] == "Hello world, etc 123 "
-    assert fragments[0][1] == py_cui.WHITE_ON_BLACK
-    assert fragments[1][0] == "@ testing @"
-    assert fragments[1][1] == py_cui.RED_ON_BLACK
-    assert fragments[2][0] == " ++-- Test"
-    assert fragments[2][1] == py_cui.WHITE_ON_BLACK
+    assert len(fragments)   == 3
+    assert fragments[0][0]  == "Hello world, etc 123 "
+    assert fragments[0][1]  == py_cui.WHITE_ON_BLACK
+    assert fragments[1][0]  == "@ testing @"
+    assert fragments[1][1]  == py_cui.RED_ON_BLACK
+    assert fragments[2][0]  == " ++-- Test"
+    assert fragments[2][1]  == py_cui.WHITE_ON_BLACK
 
     fragments, match = color_rule_A.generate_fragments(dummy_widget, test_string_B, test_string_B)
     assert match == False
-    assert len(fragments) == 1
-    assert fragments[0][0] == test_string_B
-    assert fragments[0][1] == py_cui.WHITE_ON_BLACK
+    assert len(fragments)   == 1
+    assert fragments[0][0]  == test_string_B
+    assert fragments[0][1]  == py_cui.WHITE_ON_BLACK
 
     fragments, match = color_rule_A.generate_fragments(dummy_widget, test_string_C, test_string_C)
     assert match == False
-    assert len(fragments) == 1
-    assert fragments[0][0] == test_string_C
-    assert fragments[0][1] == py_cui.WHITE_ON_BLACK
+    assert len(fragments)   == 1
+    assert fragments[0][0]  == test_string_C
+    assert fragments[0][1]  == py_cui.WHITE_ON_BLACK
 
 
 def test_generate_fragments_B():
 
     fragments, match = color_rule_B.generate_fragments(dummy_widget, test_string_A, test_string_A)
     assert match == False
-    assert len(fragments) == 1
-    assert fragments[0][0] == test_string_A
-    assert fragments[0][1] == py_cui.WHITE_ON_BLACK
+    assert len(fragments)   == 1
+    assert fragments[0][0]  == test_string_A
+    assert fragments[0][1]  == py_cui.WHITE_ON_BLACK
 
     fragments, match = color_rule_B.generate_fragments(dummy_widget, test_string_B, test_string_B)
     assert match == True
-    assert len(fragments) == 1
-    assert fragments[0][0] == test_string_B
-    assert fragments[0][1] == py_cui.RED_ON_BLACK
+    assert len(fragments)   == 1
+    assert fragments[0][0]  == test_string_B
+    assert fragments[0][1]  == py_cui.RED_ON_BLACK
 
     fragments, match = color_rule_B.generate_fragments(dummy_widget, test_string_C, test_string_C)
     assert match == False
-    assert len(fragments) == 1
-    assert fragments[0][0] == test_string_C
-    assert fragments[0][1] == py_cui.WHITE_ON_BLACK
+    assert len(fragments)   == 1
+    assert fragments[0][0]  == test_string_C
+    assert fragments[0][1]  == py_cui.WHITE_ON_BLACK
 
 
 def test_generate_fragments_C():
     fragments, match = color_rule_C.generate_fragments(dummy_widget, test_string_A, test_string_A)
     assert match == False
-    assert len(fragments) == 1
-    assert fragments[0][0] == test_string_A
-    assert fragments[0][1] == py_cui.WHITE_ON_BLACK
+    assert len(fragments)   == 1
+    assert fragments[0][0]  == test_string_A
+    assert fragments[0][1]  == py_cui.WHITE_ON_BLACK
 
     fragments, match = color_rule_C.generate_fragments(dummy_widget, test_string_B, test_string_B)
     assert match == True
-    assert len(fragments) == 3
-    assert fragments[0][0] == "     Test string number two "
-    assert fragments[0][1] == py_cui.WHITE_ON_BLACK
-    assert fragments[1][0] == "Space"
-    assert fragments[1][1] == py_cui.RED_ON_BLACK
-    assert fragments[2][0] == ''
-    assert fragments[2][1] == py_cui.WHITE_ON_BLACK
+    assert len(fragments)   == 3
+    assert fragments[0][0]  == "     Test string number two "
+    assert fragments[0][1]  == py_cui.WHITE_ON_BLACK
+    assert fragments[1][0]  == "Space"
+    assert fragments[1][1]  == py_cui.RED_ON_BLACK
+    assert fragments[2][0]  == ''
+    assert fragments[2][1]  == py_cui.WHITE_ON_BLACK
 
     fragments, match = color_rule_C.generate_fragments(dummy_widget, test_string_C, test_string_C)
     assert match == False
-    assert len(fragments) == 1
-    assert fragments[0][0] == test_string_C
-    assert fragments[0][1] == py_cui.WHITE_ON_BLACK
+    assert len(fragments)   == 1
+    assert fragments[0][0]  == test_string_C
+    assert fragments[0][1]  == py_cui.WHITE_ON_BLACK
 
 
 def test_generate_fragments_D():
--- tests/test_widget_core.py
+++ tests/test_widget_core.py
@@ -4,24 +4,28 @@ import py_cui.grid as grid
 import py_cui.widgets as widgets
 import py_cui.errors as err
 
+import py_cui.debug as dbg
 
-test_grid = grid.Grid(5, 7, 90, 210)
-test_cell_A = widgets.Widget('1', 'TestA', test_grid, 0, 0, 1, 1, 1, 0)
-test_cell_B = widgets.Widget('2', 'Test B', test_grid, 3, 4, 2, 1, 1, 0)
-test_cell_C = widgets.Widget('3', 'Test C', test_grid, 1, 2, 1, 3, 1, 0)
-test_cell_D = widgets.Widget('4', 'Test D -----------------------------------', test_grid, 0, 0, 1, 1, 1, 0)
+logger = dbg.PyCUILogger('PYCUI TEST')
+
+
+test_grid = grid.Grid(5, 7, 90, 210, logger)
+test_cell_A = widgets.Widget('1', 'TestA', test_grid, 0, 0, 1, 1, 1, 0, logger)
+test_cell_B = widgets.Widget('2', 'Test B', test_grid, 3, 4, 2, 1, 1, 0, logger)
+test_cell_C = widgets.Widget('3', 'Test C', test_grid, 1, 2, 1, 3, 1, 0, logger)
+test_cell_D = widgets.Widget('4', 'Test D -----------------------------------', test_grid, 0, 0, 1, 1, 1, 0, logger)
 
 
 # grid spot width should be 6 x 6, with an overlap of 2 chars on the edges
-test_grid_over = grid.Grid(3, 3, 20, 20)
-test_cell_over_A = widgets.Widget('5', 'Test Over A', test_grid_over, 2, 0, 1, 1, 1, 0)
-test_cell_over_B = widgets.Widget('6', 'Test Over B', test_grid_over, 0, 2, 1, 1, 1, 0)
-test_cell_over_C = widgets.Widget('7', 'Test Over C', test_grid_over, 2, 2, 1, 1, 1, 0)
+test_grid_over = grid.Grid(3, 3, 20, 20, logger)
+test_cell_over_A = widgets.Widget('5', 'Test Over A', test_grid_over, 2, 0, 1, 1, 1, 0, logger)
+test_cell_over_B = widgets.Widget('6', 'Test Over B', test_grid_over, 0, 2, 1, 1, 1, 0, logger)
+test_cell_over_C = widgets.Widget('7', 'Test Over C', test_grid_over, 2, 2, 1, 1, 1, 0, logger)
 
 
 def test_illegal_create_1():
     try:
-        test_cell_E = widgets.Widget('8', 'Test E', None, 0, 5, 1, 1, 1, 0)
+        _ = widgets.Widget('8', 'Test E', None, 0, 5, 1, 1, 1, 0, logger)
         assert False
     except err.PyCUIMissingParentError:
         assert True
@@ -29,7 +33,7 @@ def test_illegal_create_1():
 
 def test_illegal_create_2():
     try:
-        test_cell_E = widgets.Widget('9', 'Test E', test_grid, 6, 8, 1, 1, 1, 0)
+        _ = widgets.Widget('9', 'Test E', test_grid, 6, 8, 1, 1, 1, 0, logger)
         assert False
     except err.PyCUIOutOfBoundsError:
         assert True
@@ -37,7 +41,7 @@ def test_illegal_create_2():
 
 def test_illegal_create_3():
     try:
-        test_cell_E = widgets.Widget('10', 'Test E', test_grid, 4, 0, 3, 1, 1, 0)
+        _ = widgets.Widget('10', 'Test E', test_grid, 4, 0, 3, 1, 1, 0, logger)
         assert False
     except err.PyCUIOutOfBoundsError:
         assert True
@@ -45,17 +49,17 @@ def test_illegal_create_3():
 
 def test_illegal_create_4():
     try:
-        test_cell_E = widgets.Widget('11', 'Test E', test_grid, 0, 6, 1, 2, 1, 0)
+        _ = widgets.Widget('11', 'Test E', test_grid, 0, 6, 1, 2, 1, 0, logger)
         assert False
     except err.PyCUIOutOfBoundsError:
         assert True
 
 
-def test_get_absolute_position():
-    xA, yA = test_cell_A.get_absolute_position()
-    xB, yB = test_cell_B.get_absolute_position()
-    xC, yC = test_cell_C.get_absolute_position()
-    xD, yD = test_cell_D.get_absolute_position()
+def test_get_start_position():
+    xA, yA = test_cell_A.get_start_position()
+    xB, yB = test_cell_B.get_start_position()
+    xC, yC = test_cell_C.get_start_position()
+    xD, yD = test_cell_D.get_start_position()
     assert xA == -1 and yA == 1
     assert xB == 119 and yB == 55
     assert xC == 59 and yC == 19
@@ -63,10 +67,10 @@ def test_get_absolute_position():
 
 
 def test_get_absolute_dims_simple():
-    wA, hA = test_cell_A.get_absolute_dims()
-    wB, hB = test_cell_B.get_absolute_dims()
-    wC, hC = test_cell_C.get_absolute_dims()
-    wD, hD = test_cell_D.get_absolute_dims()
+    hA, wA = test_cell_A.get_absolute_dimensions()
+    hB, wB = test_cell_B.get_absolute_dimensions()
+    hC, wC = test_cell_C.get_absolute_dimensions()
+    hD, wD = test_cell_D.get_absolute_dimensions()
     assert wA == 30 and hA == 18
     assert wB == 30 and hB == 36
     assert wC == 90 and hC == 18
@@ -83,9 +87,9 @@ def test_create_with_overlap():
 def test_get_absolute_dims_overlap():
     #TODO need to fix this unit test
     return
-    wA, hA = test_cell_over_A.get_absolute_dims()
-    wB, hB = test_cell_over_B.get_absolute_dims()
-    wC, hC = test_cell_over_C.get_absolute_dims()
+    wA, hA = test_cell_over_A.get_absolute_dimensions()
+    wB, hB = test_cell_over_B.get_absolute_dimensions()
+    wC, hC = test_cell_over_C.get_absolute_dimensions()
     assert wA == 6 and hA == 8
     assert wB == 8 and hB == 6
     assert wC == 8 and hC == 8
