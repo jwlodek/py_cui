271ba6df389d7851ac623552a255f5bc07080b3d
jwlodek <jwlodek@cs.stonybrook.edu>
2020-03-20 10:48:11 -0400

    Adding some tests for py_cui core

--- tests/test_py_cui_core.py
+++ tests/test_py_cui_core.py
@@ -1,4 +1,11 @@
 import pytest
 import py_cui
+print(py_cui.__path__)
 
-#TODO
+def test_fit_text():
+    out = py_cui.fit_text(7, 'Hello World')
+    assert out == 'He...'
+
+def test_fit_text_center():
+    out = py_cui.fit_text(10, 'HI', center=True)
+    assert out == '   HI    '
