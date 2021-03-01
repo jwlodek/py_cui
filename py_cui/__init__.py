jwlodek/py_cui

commit f8d6e318f0af75c84d9a43b2bc92607955f49992

Have version in setup file pull from library

py_cui/__init__.py
@@ -41,7 +41,7 @@ import py_cui.control_widgets
 
 
 # Version number
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 
 def fit_text(width, text, center=False):

setup.py
@@ -1,6 +1,8 @@
 import setuptools
 from sys import platform
+import py_cui
 
+py_cui_version = py_cui.__version__
 
 # Use README for long description
 with open('README.md', 'r') as readme_fp:
@@ -16,7 +18,7 @@ setuptools.setup(
     description='A widget and grid based framework for building command line user interfaces in python.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='0.1.2',
+    version=py_cui_version,
     author='Jakub Wlodek',
     author_email='jwlodek.dev@gmail.com',
     license='BSD (3-clause)',

commit b7473fe3e83bea9d1df9c835db419fd6eea93bf2

Fixing bug with pip install on windows introduced with f8d6e31

py_cui/__init__.py
@@ -39,7 +39,6 @@ import py_cui.errors
 from py_cui.colors import *
 
 
-
 # Version number
 __version__ = '0.1.3'
 

setup.py
@@ -1,8 +1,5 @@
 import setuptools
 from sys import platform
-import py_cui
-
-py_cui_version = py_cui.__version__
 
 # Use README for long description
 with open('README.md', 'r') as readme_fp:
@@ -18,7 +15,7 @@ setuptools.setup(
     description='A widget and grid based framework for building command line user interfaces in python.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version=py_cui_version,
+    version='0.1.3',
     author='Jakub Wlodek',
     author_email='jwlodek.dev@gmail.com',
     license='BSD (3-clause)',